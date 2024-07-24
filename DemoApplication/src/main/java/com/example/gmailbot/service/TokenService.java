72771e3226361b42f96f0a8362f2a564fdb557a9
Emanuel Barrera <emanuelbarrera98@gmail.com>
2024-07-24 14:12:26 -0600

    a3

--- DemoApplication/src/main/java/com/example/gmailbot/service/TokenService.java
+++ DemoApplication/src/main/java/com/example/gmailbot/service/TokenService.java
@@ -1,24 +1,40 @@
 package com.example.gmailbot.service;
+
 import okhttp3.*;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Service
 public class TokenService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Value("${google.url}")
     private String url;
-    
-    @Value("${google.client.id}")
-    private String clientId;
-
-    @Value("${google.client.secret}")
-    private String clientSecret;
 
     @Value("${google.refresh.token}")
     private String refreshToken;
+
+    private String clientId;
+    private String clientSecret;
+
+    public TokenService() throws IOException {
+        // Load credentials from the JSON file
+        ClassPathResource resource = new ClassPathResource("credentials.json");
+        String jsonContent = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+        JSONObject jsonObject = new JSONObject(jsonContent);
+
+        this.clientId = jsonObject.getString("client_id");
+        this.clientSecret = jsonObject.getString("client_secret");
+    }
 
     public String getAccessToken() throws IOException {
         RequestBody formBody = new FormBody.Builder()
@@ -32,12 +48,15 @@ public class TokenService {
                 .url(url)
                 .post(formBody)
                 .build();
+
         OkHttpClient client = new OkHttpClient();
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) {
                 throw new IOException("Unexpected code " + response);
             }
-            return response.body().string();
+            String responseBody = response.body().string();
+            logger.info("Access Token: " + responseBody);
+            return responseBody;
         }
     }
 }
--- DemoApplication/src/main/java/com/example/gmailbot/task/GmailScheduledTask.java
+++ DemoApplication/src/main/java/com/example/gmailbot/task/GmailScheduledTask.java
@@ -1,28 +1,31 @@
 package com.example.gmailbot.task;
 
-import com.example.gmailbot.service.GmailService;
-import com.google.api.services.gmail.model.Message;
+import com.example.gmailbot.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class GmailScheduledTask {
 
-    @Autowired
-    private GmailService gmailService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Scheduled(fixedRate = 60000) // 1 minute interval
+    @Autowired
+    private TokenService tokenService;
+
+    @Scheduled(fixedRate = 6000) // 6 seconds interval
     public void readEmails() {
+        logger.info("hoolaa1");
+        logger.debug("holaaa2");
+        System.out.println("holaa");
         try {
-            List<Message> messages = gmailService.listMessages("me");
-            for (Message message : messages) {
-                Message fullMessage = gmailService.getMessage("me", message.getId());
-                System.out.println("Message snippet: " + fullMessage.getSnippet());
-            }
+            // Log the token
+            String accessToken = tokenService.getAccessToken();
+            System.out.println("Logged Token: " + accessToken);
         } catch (IOException e) {
             e.printStackTrace();
         }
